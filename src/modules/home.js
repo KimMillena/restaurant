58683f3ddbddb524dda385ca3662b847b2438427
Kim <kimmillena09@gmail.com>
2025-09-20 23:43:30 +0800

    Add event listener to hero menu btn

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,6 +1,9 @@
+import { menu } from "./menu.js";
 import sushiHero from "../assets/img/sushi-hero.jpg";
 
 console.log("Webpack resolved image to:", sushiHero);
+
+const pageContent = document.querySelector("#content");
 
 const createImage = () => {
     const heroImage = document.createElement("img");
@@ -31,6 +34,11 @@ const createBtn = () => {
     menuBtn.classList.add("hero-btn");
     menuBtn.textContent = "Our Menu";
 
+    menuBtn.addEventListener("click", () => { 
+        pageContent.innerHTML = ""; 
+        menu() 
+    });
+
     return menuBtn;
 };
 
@@ -56,8 +64,6 @@ const heroSection = () => {
 };
 
 export const home = () => {
-    const pageContent = document.querySelector("#content");
-
     const homeContainer = document.createElement("div");
     homeContainer.classList.add("home-container");
 
