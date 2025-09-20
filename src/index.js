33094c56f4ab71d3cab50d3c56c0ce65a2d8d805
Kim <kimmillena09@gmail.com>
2025-09-20 23:23:48 +0800

    Add event listeners to nav buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import "./styles.css";
 import { home } from "./modules/home.js";
+import { menu } from "./modules/menu.js";
+import { about } from "./modules/about";
+
+const pageContent = document.querySelector("#content");
+const homeBtn = document.querySelector(".home-btn");
+const menuBtn = document.querySelector(".menu-btn");
+const aboutBtn = document.querySelector(".about-btn");
 
 home();
+
+homeBtn.addEventListener("click", (e) => { setActivePage(e) });
+menuBtn.addEventListener("click", (e) => { setActivePage(e) });
+aboutBtn.addEventListener("click", (e) => { setActivePage(e) });
+
+
