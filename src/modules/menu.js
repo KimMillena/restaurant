0887e08abea6525af6b6e3df441363a555e5d54c
Kim <kimmillena09@gmail.com>
2025-09-21 20:01:59 +0800

    Add menu section and title

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,9 +1,28 @@
-export const menu = () => {
-    const pageContent = document.querySelector("#content");
+import sushiImg from "../assets/img/sushi-hero.jpg";
 
+const pageContent = document.querySelector("#content");
+
+const createTitle = () => {
     const menuTitle = document.createElement("h1");
     menuTitle.classList.add("menu-title");
+    menuTitle.textContent = "Popular Dishes";
 
-    menuTitle.textContent = "Our Menu";
+    return menuTitle;
+}
+
+
+
+const menuSection = () => {
+    const menuSection = document.createElement("div");
+    menuSection.classList.add("menu-section");
+
+    return menuSection;
+};
+
+export const menu = () => {
+
+    const menuTitle = createTitle();
+
+    pageContent.appendChild(menuSection());
     pageContent.appendChild(menuTitle);
 };
