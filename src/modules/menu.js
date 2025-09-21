d1f23ed4903d057170adb5d5a9e24832e4009995
Kim <kimmillena09@gmail.com>
2025-09-21 20:04:37 +0800

    Add function to create the menu cards and add placeholder data

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -10,11 +10,66 @@ const createTitle = () => {
     return menuTitle;
 }
 
+const createMenuCard = () => {
+    const menuItems = [
+        { 
+            prodName: "Sushi1", 
+            desc: "Desc1", 
+            price: "10", 
+            img: sushiImg,
+        },
+        { 
+            prodName: "Sushi2", 
+            desc: "Desc2", 
+            price: "20", 
+            img: sushiImg,
+        },
+        { 
+            prodName: "Sushi3", 
+            desc: "Desc3", 
+            price: "30", 
+            img: sushiImg,
+        },
+    ];
 
+    const menuCards = menuItems.map((item, index) => {
+        const menuCard = document.createElement("div");
+        menuCard.classList.add("menu-card");
+        menuCard.id = `menu-card${index}`;
+
+        const menuImage = document.createElement("img");
+        menuImage.classList.add("menu-img");
+        menuImage.src = item.img;
+
+        const menuName = document.createElement("h1");
+        menuName.classList.add("menu-name");
+        menuName.textContent = item.prodName;
+
+        const menuDesc = document.createElement("p");
+        menuDesc.classList.add("menu-desc");
+        menuDesc.textContent = item.desc;
+
+        const menuPrice = document.createElement("p");
+        menuPrice.classList.add("menu-price");
+        menuPrice.textContent = item.price;
+
+        menuCard.appendChild(menuImage);
+        menuCard.appendChild(menuName);
+        menuCard.appendChild(menuDesc);
+        menuCard.appendChild(menuPrice);
+
+        return menuCard;
+    });
+
+    return menuCards;
+};
 
 const menuSection = () => {
     const menuSection = document.createElement("div");
     menuSection.classList.add("menu-section");
+
+    const menuCards = createMenuCard();
+    menuCards.forEach((card) => menuSection.appendChild(card));
 
     return menuSection;
 };
