6904bec7800928a87da7966a8733864f843b32f9
Kim <kimmillena09@gmail.com>
2025-09-23 22:13:12 +0800

    Add new foods to menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,7 +1,9 @@
 import sushiImg from "../assets/img/sushi-1858696_1920.jpg";
-import sushiImg1 from "../assets/img/food-1406883_1920.jpg";
-import sushiImg2 from "../assets/img/maki-roll-2186518_1920.jpg";
-import sushiImg3 from "../assets/img/sushi-2714101_1920.jpg";
+import sushiImg1 from "../assets/img/sushi-1406883_1920.jpg";
+import sushiImg2 from "../assets/img/sushi-roll-2186518_1920.jpg";
+import sushiImg3 from "../assets/img/sushi-5002639_1920.jpg";
+import sushiImg4 from "../assets/img/sushi-2714101_1920.jpg";
+import sushiImg5 from "../assets/img/sushi-2541472_1920.jpg";
 
 const pageContent = document.querySelector("#content");
 
@@ -19,29 +21,43 @@ const createMenuCard = () => {
       prodName: "Sāmon Sashimi Mori",
       prodSubName: "Salmon Sashimi Platter",
       desc: "Fresh salmon slices served with soy sauce and garnish.",
-      price: "¥680",
+      price: "¥1,180",
       img: sushiImg,
     },
     {
       prodName: "Kuro Goma Maki",
       prodSubName: "Black Sesame Roll",
       desc: "Crab and avocado roll topped with black sesame seeds.",
-      price: "¥520",
+      price: "¥1,120",
       img: sushiImg1,
     },
     {
       prodName: "Ikura Roll",
       prodSubName: "Salmon Roe Roll",
       desc: "Crab and cucumber roll topped with vibrant ikura (salmon roe) and creamy sauce. Served with pickled ginger and wasabi.",
-      price: "¥620",
+      price: "¥1,020",
       img: sushiImg2,
+    },
+    {
+      prodName: "Oishii Nigiri Platter",
+      prodSubName: "Chef’s Seasonal Selection",
+      desc: "A vibrant assortment of nigiri and rolls featuring salmon, tuna, roe, and fresh garnishes. Includes wasabi, daikon, and dipping sauce.",
+      price: "¥2,980",
+      img: sushiImg3,
     },
     {
       prodName: "Tempura Roll",
       prodSubName: "Tempura Crunch Roll",
       desc: "Shrimp tempura roll with spicy mayo and sweet glaze.",
-      price: "¥580",
-      img: sushiImg3,
+      price: "¥1,280",
+      img: sushiImg4,
+    },
+    {
+      prodName: "Avocado Dragon Roll",
+      prodSubName: "Eel & Avocado Harmony",
+      desc: "Eel roll topped with creamy avocado, sweet glaze, and sesame seeds. Served with pickled ginger.",
+      price: "¥1,480",
+      img: sushiImg5,
     },
   ];
 
