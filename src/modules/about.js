36543f6df14a4426fe348b5d1889914f1d67a632
Kim <kimmillena09@gmail.com>
2025-09-23 21:15:05 +0800

    Add new content to about

--- src/modules/about.js
+++ src/modules/about.js
@@ -13,10 +13,22 @@ const createTitle = () => {
 const createDesc = () => {
   const aboutDesc = document.createElement("p");
   aboutDesc.classList.add("about-desc");
-  aboutDesc.textContent =
-    "At Oishii Sushi, we believe sushi is more than a dish—it's a story told through flavor, texture, and tradition. Rooted in Japanese culinary artistry and inspired by modern tastes, our menu blends premium ingredients with thoughtful presentation. From delicate sashimi to bold fusion rolls, every plate is crafted to honor authenticity while inviting discovery. Whether you're a seasoned sushi lover or exploring it for the first time, we welcome you to experience the harmony of freshness, creativity, and care in every bite.";
+  aboutDesc.id = "about-desc";
+  aboutDesc.textContent = `At Oishii Sushi, we believe sushi is more than a dish—it’s a story told through flavor, texture, and tradition. Rooted in Japanese culinary artistry and inspired by modern tastes, our menu blends premium ingredients with thoughtful presentation.
+     From delicate sashimi to bold fusion rolls, every plate is crafted to honor authenticity while inviting discovery.`;
 
-  return aboutDesc;
+  const aboutDesc1 = document.createElement("p");
+  aboutDesc1.classList.add("about-desc");
+  aboutDesc1.id = "about-desc1";
+  aboutDesc1.textContent = `Each piece is a moment—crisply rolled, precisely sliced, and delicately dipped—designed to awaken the senses and evoke the spirit of omotenashi, the Japanese art of wholehearted hospitality. 
+     We source only the freshest seafood and seasonal produce, ensuring that every bite reflects the harmony of nature and the precision of technique.`;
+
+  const aboutDesc2 = document.createElement("p");
+  aboutDesc2.classList.add("about-desc");
+  aboutDesc2.id = "about-desc2";
+  aboutDesc2.textContent = `Whether you're a seasoned sushi lover or exploring it for the first time, we welcome you to experience the balance of freshness, creativity, and care in every bite. From the first cut to the final dip, Oishii Sushi is where tradition meets innovation—on your plate.`;
+
+  return [aboutDesc, aboutDesc1, aboutDesc2];
 };
 
 const imageContainer = () => {
@@ -64,7 +76,7 @@ export const about = () => {
   const aboutDesc = createDesc();
 
   leftContainer.appendChild(aboutTitle);
-  leftContainer.appendChild(aboutDesc);
+  aboutDesc.forEach((desc) => leftContainer.appendChild(desc));
   aboutContainer.appendChild(leftContainer);
   aboutContainer.appendChild(imgContainer);
 
